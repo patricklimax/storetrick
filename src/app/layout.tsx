040dec3755c7ad5e4e6d6e62fffa8101d0965d32
patricklimax <patricklimax@outlook.com.br>
2024-03-12 11:09:47 -0300

    inital setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google"
+import "./globals.css";
+import { cn } from '@/lib/utils';
+import { ThemeProvider } from '@/components/theme-provider';
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export const metadata: Metadata = {
+  title: "Trick Car",
+  description: "Seu sonho está aqui",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR" suppressHydrationWarning>
+      <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { ThemeToogle } from '@/components/themeToogle';
+
+export default function Home() {
+  return (
+    <main className="">
+      <ThemeToogle/>
+      <div>Patrick Lima</div>
+    </main>
+  );
+}
